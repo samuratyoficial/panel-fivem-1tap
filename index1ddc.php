94ab7b0f7428d454c753356449325659b5d5b991
SAMURATY <76045309+samuratyoficial@users.noreply.github.com>
2022-01-11 21:38:43 +0200

    Add files via upload

---
+++ index1ddc.php
@@ -0,0 +1,5 @@
+{
+    "scope": "\/",
+    "name": "1TAP.RO - #1 Comunitate Gaming Romania",
+    "theme_color": "#3d6594"
+}
---
+++ index1e49.php
@@ -0,0 +1,347 @@
+<?xml version="1.0"?>
+<rss version="2.0"><channel><title>All Activity</title><link>https://1tap.ro/forum/index.php?/discover/</link><description>1TAP.RO - #1 Comunitate Gaming Romania - All Activity</description><language>ro</language><item><title>Uniforma</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28441-uniforma/&do=findComment&comment=87574]]></link><description>Tricou politie ordine publica bleumarin cu intersie si epoleti;</description><pubDate>Sat, 14 Aug 2021 20:18:20 +0000</pubDate></item><item><title>Uniforma</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28441-uniforma/&do=findComment&comment=87573]]></link><description>Uniforma jandarmi sau fortele de ordine ( Jandarmeria Romana ) fabricata din material de inalta calitate si adecvata pentru toate conditiile !</description><pubDate>Sat, 14 Aug 2021 20:13:27 +0000</pubDate></item><item><title>[Prezentare] Andreass77777</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28440-prezentare-andreass77777/&do=findComment&comment=87572]]></link><description>hai noroc.</description><pubDate>Mon, 09 Aug 2021 17:45:51 +0000</pubDate></item><item><title>[Prezentare] Andreass77777</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28440-prezentare-andreass77777/&do=findComment&comment=87571]]></link><description>Salut si bine ai venit in comunitatea 1TAP.
+ 
+
+
+	Sa ai o sedere placuta, speram sa te simti ca acasa</description><pubDate>Sat, 07 Aug 2021 00:45:46 +0000</pubDate></item><item><title>[Prezentare] Andreass77777</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28440-prezentare-andreass77777/&do=findComment&comment=87570]]></link><description><![CDATA[Prenume :  Andreas
+	
+	
+		Nickname CS :  andreass77777
+	
+	
+		Vârsta : 15
+	
+	
+		Locația : Bistrita
+	
+	
+		Ocupație : Elev
+	
+	
+		Hobby-uri și pasiuni : Downhill&amp;Freeride, Muzica, Natura, Voluntariatul
+	
+	
+		Mod preferat : Arena
+	
+	
+		Steam : https://steamcommunity.com/id/andreass77777/
+	
+	
+		De unde ai auzit de 1TAP România? :  Am aflat de 1TAP România intamplator, in timp ce cautam un server de CSGO interesant pe care sa ma distrez, si am gasit exact ce cautam
+	
+	
+		Acordă o notă comunității noastre : 9
+	
+	
+		Contact : Instagram : @andreass77777
+	
+	
+		Alte detalii : -]]></description><pubDate>Tue, 03 Aug 2021 18:15:29 +0000</pubDate></item><item><title>After getting the most recent update, the players of Call of Duty Modern Warfare are </title><link><![CDATA[https://1tap.ro/forum/index.php?/profile/13899-android-gram/&status=167&type=status]]></link><description>After getting the most recent update, the players of Call of Duty Modern Warfare are facing the Dev Error 5573
+ 
+
+
+	 
+ 
+
+
+	
+		
+			
+				https://androidgram.com/dev-error-5573-ps5-how-to-fix/</description><pubDate>Tue, 03 Aug 2021 04:18:10 +0000</pubDate></item><item><title>Model Raportare Bug-uri</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28438-model-raportare-bug-uri/&do=findComment&comment=87568]]></link><description>1. Nickname: 
+ 
+
+
+	2. Problema sesizata: 
+ 
+
+
+	3. O scurta descriere despre cum poate fi reprodus respectivul bug: 
+ 
+
+
+	4. Screenshot/Videoclip care sa prezinte respectivul bug:
+ 
+
+
+	5. Adresa de discord: 
+ 
+
+
+	6. Alte comentarii: -</description><pubDate>Fri, 30 Jul 2021 13:29:47 +0000</pubDate></item><item><title>Model Cerere UnBan</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28437-model-cerere-unban/&do=findComment&comment=87567]]></link><description>1) Nickname-ul tau:
+ 
+
+
+	2) Nickname-ul celui care &#x21B;i-a dat ban:
+ 
+
+
+	3) Motivul pentru care ai fost banat:
+ 
+
+
+	4) Screenshot cu mesajul care apare cand incerci sa intri (Obligatoriu cu F3 ON):
+ 
+
+
+	5) De ce crezi ca ar trebui sa prime&#x219;ti unban?:
+ 
+
+
+	6) Comentarii adi&#x21B;ionale:</description><pubDate>Fri, 30 Jul 2021 13:26:08 +0000</pubDate></item><item><title>Model Reclamatii Staff</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28435-model-reclamatii-staff/&do=findComment&comment=87565]]></link><description>1. Nickname-ul t&#x103;u:
+ 
+
+
+	2. Nickname-ul celui reclamat:
+ 
+
+
+	4. Motivul pentru care &#xEE;l reclami:
+ 
+
+
+	5. Dovada
+ 
+
+
+	6. Comentarii adi&#x21B;ionale:</description><pubDate>Fri, 30 Jul 2021 13:22:57 +0000</pubDate></item><item><title>Model reclamatii  Jucator</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28434-model-reclamatii-jucator/&do=findComment&comment=87564]]></link><description>1. Nickname-ul t&#x103;u:
+ 
+
+
+	2. Nickname-ul persoanei reclamate:
+ 
+
+
+	3. Motivul pentru care &#xEE;l reclami:
+ 
+
+
+	4. Dovada:</description><pubDate>Fri, 30 Jul 2021 13:17:28 +0000</pubDate></item><item><title>Lista Staff</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28433-lista-staff/&do=findComment&comment=87563]]></link><description>Lista Staff Danger Academy  
+	 
+	 
+	 
+	 
+	                                                                                                                                                                                                       Owner 
+	                                                                                                                                                                                                  Taka_Yoko 
+	 
+	                                                                                                                                                                                              Administrator 
+	                                                                                                                                                                                                        NONE 
+	 
+	                                                                                                                                                                                                     Manager 
+	                                                                                                                                                                                                        NONE 
+	 
+	                                                                                                                                                                                                   Supervizor 
+	                                                                                                                                                                                                       NONE 
+	 
+	                                                                                                                                                                                                      Operator 
+	                                                                                                                                                                                                        NONE 
+	 
+	                                                                                                                                                                                                       Admin 
+	                                                                                                                                                                                                        NONE 
+	 
+	                                                                                                                                                                                                   Moderator 
+	                                                                                                                                                                                                        NONE 
+	 
+ 
+
+
+	                                                                                                                                                                                                         Builder 
+	                                                                                                                                                                                                     DanterYTB  
+	 
+	                                                                                                                                                                                                        Helper 
+	                                                                                                                                                                                                         NONE 
+	 
+	                                                                                                                                                                                                    Trial-Helper 
+	                                                                                                                                                                                                          Sfyki 
+	                                                                                                                                                                                                      kinezuuFTW 
+	                                                                                                                                                                                                          Hudi54 
+	                                                                                                                                                                                                    HatzzzJhonu</description><pubDate>Fri, 30 Jul 2021 13:11:04 +0000</pubDate></item><item><title>Regulament</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28432-regulament/&do=findComment&comment=87562]]></link><description>Regulament DangerAcademy                                                                                                                                      
+	 
+	 
+ 
+
+
+	1. S&#x103; prelungi&#x21B;i cuvinte sau sintaxe pe chat-uri publice, /helpop sau /donorchat. (Ex.: saaaaaaaaalut; 777777777; 10918123496; ahahahahahahah). Prelungirea se va sanctiona de la 9 caractere in sus. 
+	MUTE 10 MINUTE
+ 
+
+
+	2. S&#x103; face&#x21B;i off topic, spam, s&#x103; folosti caps sau s&#x103; &#xEE;njura&#x21B;i pe /helpop. Juc&#x103;torii pot folosi acel chat doar pentru a adresa echipei noastre &#xEE;ntreb&#x103;ri ce au leg&#x103;tur&#x103; cu server-ul/jocul. De asemenea, acelasi lucru se aplica si pe /dc, chat-ul special pentru donatori. 
+	 WARN
+ 
+
+
+	3. S&#x103; raporta&#x21B;i hackeri pe chat-ul public, raportearea acestora se face numai pe /helpop pentru ca echipa noastr&#x103; s&#x103; &#xEE;i poat&#x103; prinde mai u&#x219;or. 
+	 WARN
+ 
+
+
+	4. S&#x103; amenintati un Jucator sau Donator. Orice tip de amenintare se sanctioneaza, iar o exceptie de la aceasta regula este in cazul membrilor staff. 
+	MUTE 20 MINUTE
+ 
+
+
+	5. S&#x103; scrie&#x21B;i mai mult de 3 cuvinte cu CapsLock activat, chiar dac&#x103; nu sunt pe acela&#x219;i r&#xE2;nd. &#xCE;n cazul &#xEE;n care cuvintele sunt scrise cu CapsLock activat, dar pe r&#xE2;nduri diferite, sanc&#x21B;iunea va fi aceea&#x219;i, dar pe motivul CapsLock Avoid. 
+	 MUTE 10 MINUTE
+ 
+
+
+	6. S&#x103; face&#x21B;i afaceri &#xEE;n afara jocului (ex.:"V&#xE2;nd cont premium pentru Key GTA 5 pe Steam"; "Dau 1M pe Factions pentru un cont de Minecraft Premium!"). Singurele schimburi permise sunt cele care con&#x21B;in pachete disponibile pe STORE. 
+	MUTE 1 Ora
+ 
+
+
+	7. S&#x103; promova&#x21B;i site-uri, canale de YouTube, servere de Discord ce nu au leg&#x103;tur&#x103; cu serverul sau comunitatea noastr&#x103;. De asemenea, sintaxe precum "Caut server", "Cine are server?" sau men&#x21B;ionarea altor servere/comunit&#x103;&#x21B;i de Minecraft se &#xEE;ncadreaz&#x103; &#xEE;n aceast&#x103; regul&#x103; &#x219;i se vor sanc&#x21B;iona la fel. 
+	MUTE 4 ORE
+ 
+
+
+	8. S&#x103; aduce&#x21B;i insulte asupra serverului, a comunitatii sau a personalului. 
+	MUTE 1 ZI
+ 
+
+
+	9. S&#x103; intra&#x21B;i cu 2+ conturi la event. &#xCE;n cazul &#xEE;n care ave&#x21B;i fratii pe acela&#x219;i IP, va trebui s&#x103; anun&#x21B;a&#x21B;i operatorul sectional &#xEE;nainte de &#xEE;nceperea evenimentului. 
+	BAN 6 ORE
+ 
+
+
+	10.S&#x103; face&#x21B;i trap. Orice fel de trap este interzis, de asemenea trap-ul va fii spart de un Operator. 
+	JAIL 10 ORE
+ 
+
+
+	11. S&#x103; instigati ceilal&#x21B;i juc&#x103;tori la sanc&#x21B;iune (ex: Cine &#xEE;l &#xEE;njur&#x103; pe Marian primeste 15k). 
+	BAN 2 ORE
+ 
+
+
+	12. S&#x103; face&#x21B;i construc&#x21B;ii obscene 
+	BAN 12 ORE + CONSTRUC&#x21A;IE STEARS&#x102;</description><pubDate>Fri, 30 Jul 2021 12:27:23 +0000</pubDate></item><item><title>Model cerere YouTuber</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28431-model-cerere-youtuber/&do=findComment&comment=87561]]></link><description>DangerAcademy                                                                                                                                      
+	Nume?: 
+	Numele in joc?: 
+	Cati ani ai?: 
+	ID Discord: 
+	Cati abonati ai(minim 30)?: 
+	Descrie-te in minim 20 de cuvinte:</description><pubDate>Fri, 30 Jul 2021 01:16:08 +0000</pubDate></item><item><title>Model Cerere TrialHelper</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28430-model-cerere-trialhelper/&do=findComment&comment=87560]]></link><description>Cum te Numesti?: 
+	Nume pe joc?: 
+	Cati ani ai?: 
+	ID discord?: 
+	De ce sa te alegem pe tine?: 
+	Care este rolul principal al unui TrialHelper?: 
+	Descrie-te &#xEE;n minim 25 cuvinte: 
+	Ce po&#x21B;i s&#x103; aduci &#xEE;n plus Staf-ului?:</description><pubDate>Fri, 30 Jul 2021 01:09:26 +0000</pubDate></item><item><title>Model Cerere Builder</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28429-model-cerere-builder/&do=findComment&comment=87559]]></link><description>DangerAcademy                                                                                                                                  
+	 
+	Nume?: 
+	Cati ani anI? 
+	Nume joc?: 
+	Experienta in Build?: 
+	Clasa de Build (Structura/Terraforming/Organic)?: 
+	Cat timp ai stat la cea mai lunga constructie?: 
+	Niste poze la constructile tale: 
+	Alte precizari?:</description><pubDate>Fri, 30 Jul 2021 01:08:15 +0000</pubDate></item><item><title>Cerere DNS dangeracademy.1tap.ro</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28426-cerere-dns-dangeracademy1tapro/&do=findComment&comment=87556]]></link><description>Categoriile au fost creeate.
+ 
+
+
+	DNS-ul va fii disponibil in cel mai scurt timp.
+ 
+
+
+	Te-as ruga sa-mi dai ADD pe discord deoarece eu nu am reusit, ai discord-ul meu la mine pe profil la BANNER.
+ 
+
+
+	T/C !</description><pubDate>Tue, 27 Jul 2021 17:59:01 +0000</pubDate></item><item><title>Cerere DNS dangeracademy.1tap.ro</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28426-cerere-dns-dangeracademy1tapro/&do=findComment&comment=87555]]></link><description>De&#x21B;in&#x103;tori (numele de pe forum) : TakaYoko*</description><pubDate>Sun, 25 Jul 2021 13:27:26 +0000</pubDate></item><item><title>Cerere DNS dangeracademy.1tap.ro</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28426-cerere-dns-dangeracademy1tapro/&do=findComment&comment=87554]]></link><description>Numele d-vs. : Andresi Andrei
+	
+	
+		IP server : 193..84.64.45:25565
+	
+	
+		DNS : dangeracademy.1tap.ro
+	
+	
+		Tipul jocului : Minecraft
+	
+	
+		Modul serverului : Survival
+	
+	
+		Num&#x103;r de sloturi : 35
+	
+	
+		Firm&#x103; g&#x103;zduire : gazduirejocuri.ro
+	
+	
+		De&#x21B;in&#x103;tori (numele de pe forum) : TakaYoko Regulament Server, Cerere Helper, Reckanatii Staff, Cerere Un-Ban, Lista Staff, Preturi, Discord
+	
+	
+		Structur&#x103; Sub-Forum : Regulament Server, Cerere Helper, Reckanatii Staff, Cerere Un-Ban, Lista Staff, Preturi, Discord
+	
+	
+		Link GameTracker : https://www.gametracker.com/server_info/193.84.64.45:25565/
+	
+	
+		Contact : Discord: .YoYoU  &#xFF89;&#x1D30;&#x1D3A;&#x1D33;#0001
+	
+	
+		Recomandat de (numele persoanei care va recomandat forum-ul) :-</description><pubDate>Sun, 25 Jul 2021 12:52:11 +0000</pubDate></item><item><title>De ce inchiderea serverului jb.1tap.ro?</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28425-de-ce-inchiderea-serverului-jb1tapro/&do=findComment&comment=87553]]></link><description>Salut,
+ 
+
+
+	Din pacate nu am cunostiinte legate de acest aspect.
+ 
+
+
+	Dar pot presupune ca asa a decis fondatorul serverului.</description><pubDate>Sat, 17 Jul 2021 02:20:33 +0000</pubDate></item><item><title>De ce inchiderea serverului jb.1tap.ro?</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28425-de-ce-inchiderea-serverului-jb1tapro/&do=findComment&comment=87552]]></link><description>de ce serverul jb.1tap.ro s-a &#xEE;nchis?</description><pubDate>Fri, 16 Jul 2021 12:45:25 +0000</pubDate></item><item><title>[STRIKE] Ban List Model</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28387-strike-ban-list-model/&do=findComment&comment=87551]]></link><description>Nume: Uchiha Madara
+ 
+
+
+	SteamID: STEAM_1:1:608191228
+ 
+
+
+	Timp: permanent
+ 
+
+
+	Motiv: hack(skin changer, aim)
+ 
+
+
+	Dovezi(demo/yt video/etc):https://ibb.co/dkq1Q8D http:// https://ibb.co/Bqms8t0 https://ibb.co/fkmJ16B</description><pubDate>Wed, 14 Jul 2021 21:30:58 +0000</pubDate></item><item><title>Cerere unban</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28424-cerere-unban/&do=findComment&comment=87550]]></link><description>Am revizuit demo-ul cu echipa staff si ban-ul ramane.</description><pubDate>Wed, 14 Jul 2021 19:09:12 +0000</pubDate></item><item><title>Cerere unban</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28424-cerere-unban/&do=findComment&comment=87549]]></link><description>Salut! 
+ 
+
+
+	Am revizuit demo impreuna cu echipa staff si am fost toti de comun acord ca banul este justificat
+ 
+
+
+	Ne pare rau ,incearca sa nu mai folosesti coduri in viitor 
+ 
+
+
+	Pentru unban poti plati suma de 5 euro daca vrei sa mai joci pe server
+ 
+
+
+	 
+ 
+
+
+	                                                                                                                                     Cu drag echipa staff, awp.1tap.ro
+ 
+
+
+	  
+ 
+
+
+	                                                                                                                                                                            Admin: g0g</description><pubDate>Tue, 13 Jul 2021 20:11:28 +0000</pubDate></item><item><title>Cerere unban</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28424-cerere-unban/&do=findComment&comment=87548]]></link><description>Am primit si eu ban pe sv de awp pe motiv de wall din cate am inteles adminul care mi-a dat ban nu are demo da inca nu stiu numele celui care ma banat as dori si eu unban pentru un motiv care nu are exista pentru ca nu ma joc cu coduri  
+	steam:https://steamcommunity.com/id/Cristi-/</description><pubDate>Tue, 13 Jul 2021 15:15:17 +0000</pubDate></item><item><title>cerere dns OLDCS.1TAP.RO</title><link><![CDATA[https://1tap.ro/forum/index.php?/topic/28423-cerere-dns-oldcs1tapro/&do=findComment&comment=87547]]></link><description>Redirect-ul catre serverul de discord a fost creeat .
+ 
+
+
+	Cand va fii gata DNS-ul vei primii un mesaj privat pe discord.
+ 
+
+
+	T / C !</description><pubDate>Sun, 11 Jul 2021 06:03:16 +0000</pubDate></item></channel></rss>
